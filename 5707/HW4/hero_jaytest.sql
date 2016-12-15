60dac728dd9ad25437b6a667ce6f9cc26a379c9e
fairjay8788 <fairjay8788@gmail.com>
2016-12-14 18:24:29 -0600

    Update hero_jaytest.sql

--- 5707/HW4/hero_jaytest.sql
+++ 5707/HW4/hero_jaytest.sql
@@ -1,90 +1,95 @@
---Which team has a higher average attack value, Avengers or X-men?
+--Which team has the highest average attack value, Avengers, X-men, Shiled or Spider-Friends? (Ignore the cards with a "Null" in ATTACK)
 /**
-drop table JTEST_TEMP1;
-drop table JTest_Team_Avg_Attack;
-exec ops.go(ops.filter_ra('HERO_CARD','TEAM is not null','JTEST_TEMP1'));
-exec ops.go(ops.group_ra('JTEST_TEMP1','TEAM','Avg_Attack=avg(ATTACK)','JTest_Team_Avg_Attack'));
+drop table HW4_HERO_CARD_W_TEAM;
+drop table HW4_Team_Avg_Attack;
+exec ops.go(ops.filter_ra('HERO_CARD','TEAM is not null','HW4_HERO_CARD_W_TEAM'));
+exec ops.go(ops.group_ra('HW4_HERO_CARD_W_TEAM','TEAM','Avg_Attack=avg(ATTACK)','HW4_Team_Avg_Attack'));
 /**/
---Which class is cheapest in general?
+--Which class is cheapest in general?(Ignore the cards with a "Null" in COST)
 /**
-drop table JTest_Class_Avg_COST;
-exec ops.go(ops.group_ra('HERO_CARD','CLASS','Avg_COST=avg(COST)','JTest_Class_Avg_COST'));
+drop table HW4_Class_Avg_COST;
+exec ops.go(ops.group_ra('HERO_CARD','CLASS','Avg_COST=avg(COST)','HW4_Class_Avg_COST'));
 /**/
 --Find each female hero who belongs to at least Ranged and Reflexes classes
 /**
-drop table JTEST_HERO_CARD_W_GENDER;
-drop table JTEST_HERO_CARD_FEMALE;
-drop table JTEST_HERO_CARD_FEMALE_RR;
-drop table JTEST_TEMP2;
-drop table JTEST_TEMP3;
-drop table JTEST_FEMALE_HERO_CHAR_RR;
-exec ops.go(ops.mjoin_ra('HERO_CARD','HERO','HERO','HERO_NAME','JTEST_HERO_CARD_W_GENDER'));
-exec ops.go(ops.filter_ra('JTEST_HERO_CARD_W_GENDER','HERO_GENDER=''Female''','JTEST_HERO_CARD_FEMALE'));
-exec ops.go(ops.filter_ra('JTEST_HERO_CARD_FEMALE','CLASS=''Ranged'' or CLASS=''Reflexes''','JTEST_HERO_CARD_FEMALE_RR'));
-exec ops.go(ops.group_ra('JTEST_HERO_CARD_FEMALE_RR','HERO','CLASS_COUNT=count(DISTINCT CLASS)','JTEST_TEMP2'));
-exec ops.go(ops.filter_ra('JTEST_TEMP2','CLASS_COUNT=2','JTEST_TEMP3'));
-exec ops.go(ops.reduce_ra('JTEST_TEMP3','HERO','JTEST_FEMALE_HERO_CHAR_RR'));
+drop table HW4_HERO_CARD_W_GENDER;
+drop table HW4_HERO_CARD_FEM_RAN_OR_REF;
+drop table HW4_HERO_CARD_FEM_RRCLAS_CNT;
+drop table HW4_HERO_CARD_FEM_RAN_N_REF;
+drop table HW4_HERO_FEM_RAN_N_REF;
+exec ops.go(ops.mjoin_ra('HERO_CARD','HERO','HERO','HERO_NAME','HW4_HERO_CARD_W_GENDER'));
+exec ops.go(ops.filter_ra('HW4_HERO_CARD_W_GENDER','HERO_GENDER=''Female'' and (CLASS=''Ranged'' or CLASS=''Reflexes'')','HW4_HERO_CARD_FEM_RAN_OR_REF'));
+exec ops.go(ops.group_ra('HW4_HERO_CARD_FEM_RAN_OR_REF','HERO','CLASS_COUNT=count(DISTINCT CLASS)','HW4_HERO_CARD_FEM_RRCLAS_CNT'));
+exec ops.go(ops.filter_ra('HW4_HERO_CARD_FEM_RRCLAS_CNT','CLASS_COUNT=2','HW4_HERO_CARD_FEM_RAN_N_REF'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_FEM_RAN_N_REF','HERO','HW4_HERO_FEM_RAN_N_REF'));
 /**/
---Find each pair of male and female hero who are from different team but same class
+--Find each pair of male and female hero who are from different teams but same class
 /**
-exec ops.go(ops.mjoin_ra('HERO_CARD','HERO','HERO','HERO_NAME','JTEST_HERO_CARD_W_GENDER'));
-exec ops.go(ops.filter_ra('JTEST_HERO_CARD_W_GENDER','HERO_GENDER=''Male''','JTEST_HERO_MALE'));
-exec ops.go(ops.filter_ra('JTEST_HERO_CARD_W_GENDER','HERO_GENDER=''Female''','JTEST_HERO_FEMALE'));
-exec ops.go(ops.times_ra('JTEST_HERO_MALE','JTEST_HERO_FEMALE','JTEST_HERO_GENDERMIX_PAIR'));
-exec ops.go(ops.filter_ra('JTEST_HERO_GENDERMIX_PAIR','JTEST_HERO_MALE_TEAM!=JTEST_HERO_FEMALE_TEAM and JTEST_HERO_MALE_CLASS=JTEST_HERO_FEMALE_CLASS','JTEST_HERO_MF_TC'));
---exec ops.go(ops.project_ra('JTEST_HERO_MF_TC','JTEST_HERO_MALE_HERO,JTEST_HERO_MALE_TEAM,JTEST_HERO_MALE_CLASS,JTEST_HERO_FEMALE_HERO,JTEST_HERO_FEMALE_TEAM,JTEST_HERO_FEMALE_CLASS,JTEST_HERO_MALE_NAME,JTEST_HERO_FEMALE_NAME','JTEST_HERO_MF_TC_FINAL'));
-exec ops.go(ops.reduce_ra('JTEST_HERO_MF_TC','JTEST_HERO_MALE_HERO,JTEST_HERO_MALE_TEAM,JTEST_HERO_MALE_CLASS,JTEST_HERO_FEMALE_HERO,JTEST_HERO_FEMALE_TEAM,JTEST_HERO_FEMALE_CLASS','JTEST_HERO_MF_TC_FINAL'));
+drop table HW4_HERO_CARD_W_GENDER;
+drop table HW4_HERO_CARD_MALE;
+drop table HW4_HERO_CARD_FEMALE;
+drop table HW4_HERO_CARD_GENDERMIX_PAIR;
+drop table HW4_HERO_CARD_GEN_CLA_TEAM;
+drop table HW4_HERO_CARD_GEN_CLA_TEAM_RES;
+exec ops.go(ops.mjoin_ra('HERO_CARD','HERO','HERO','HERO_NAME','HW4_HERO_CARD_W_GENDER'));
+exec ops.go(ops.filter_ra('HW4_HERO_CARD_W_GENDER','HERO_GENDER=''Male''','HW4_HERO_CARD_MALE'));
+exec ops.go(ops.filter_ra('HW4_HERO_CARD_W_GENDER','HERO_GENDER=''Female''','HW4_HERO_CARD_FEMALE'));
+exec ops.go(ops.times_ra('HW4_HERO_CARD_MALE','HW4_HERO_CARD_FEMALE','HW4_HERO_CARD_GENDERMIX_PAIR'));
+exec ops.go(ops.filter_ra('HW4_HERO_CARD_GENDERMIX_PAIR','HW4_HERO_CARD_MALE_TEAM!=HW4_HERO_CARD_FEMALE_TEAM and HW4_HERO_CARD_MALE_CLASS=HW4_HERO_CARD_FEMALE_CLASS','HW4_HERO_CARD_GEN_CLA_TEAM'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_GEN_CLA_TEAM','MALE_HERO=HW4_HERO_CARD_MALE_HERO,MALE_HERO_TEAM=HW4_HERO_CARD_MALE_TEAM,FEMALE_HERO=HW4_HERO_CARD_FEMALE_HERO,FEMALE_HERO_TEAM=HW4_HERO_CARD_FEMALE_TEAM,CLASS=HW4_HERO_CARD_FEMALE_CLASS','HW4_HERO_CARD_GEN_CLA_TEAM_RES'));
 /**/
 --Suppose you have 3 cards in hand. You want to use all of them to recruite from the rest of the cards. Find each set of three cards that would make it possible 
 --for you to recruite at least one card from each class.
 /**
-drop table JTEST_HERO_CARD_STRENGTH;
-drop table JTEST_HERO_CARD_REFLEXES;
-drop table JTEST_HERO_CARD_RANGED;
-drop table JTEST_HERO_CARD_INTUITION;
-drop table JTEST_HERO_CARD_TECH;
-drop table JTEST_HERO_CARD_STRENGTH1;
-drop table JTEST_HERO_CARD_REFLEXES1;
-drop table JTEST_HERO_CARD_RANGED1;
-drop table JTEST_HERO_CARD_INTUITION1;
-drop table JTEST_HERO_CARD_TECH1;
-drop table JTEST_HERO_CARD_SR;
-drop table JTEST_HERO_CARD_SRR;
-drop table JTEST_HERO_CARD_SRRI;
-drop table JTEST_HERO_CARD_CLASS_MIX;
-drop table JTEST_HERO_CARD_CLASS_MIX1;
+drop table HW4_HERO_CARD_STR;
+drop table HW4_HERO_CARD_REF;
+drop table HW4_HERO_CARD_RAN;
+drop table HW4_HERO_CARD_INT;
+drop table HW4_HERO_CARD_TEC;
+drop table HW4_HERO_CARD_STR_CLA_COST;
+drop table HW4_HERO_CARD_REF_CLA_COST;
+drop table HW4_HERO_CARD_RAN_CLA_COST;
+drop table HW4_HERO_CARD_INT_CLA_COST;
+drop table HW4_HERO_CARD_TEC_CLA_COST;
+drop table HW4_HERO_CARD_CLASS_SR;
+drop table HW4_HERO_CARD_CLASS_SRR;
+drop table HW4_HERO_CARD_CLASS_SRRI;
+drop table HW4_HERO_CARD_CLASS_SRRIT;
+drop table HW4_HERO_CARD_CLASS_MIX_W_COST;
 
+exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Strength''','HW4_HERO_CARD_STR'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_STR','NAME1=NAME','COST1=COST,CLASS1=CLASS','HW4_HERO_CARD_STR_CLA_COST'));
+exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Reflexes''','HW4_HERO_CARD_REF'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_REF','NAME2=NAME','COST2=COST,CLASS2=CLASS','HW4_HERO_CARD_REF_CLA_COST'));
+exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Ranged''','HW4_HERO_CARD_RAN'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_RAN','NAME3=NAME','COST3=COST,CLASS3=CLASS','HW4_HERO_CARD_RAN_CLA_COST'));
+exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Intuition''','HW4_HERO_CARD_INT'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_INT','NAME4=NAME','COST4=COST,CLASS4=CLASS','HW4_HERO_CARD_INT_CLA_COST'));
+exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Tech''','HW4_HERO_CARD_TEC'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_TEC','NAME5=NAME','COST5=COST,CLASS5=CLASS','HW4_HERO_CARD_TEC_CLA_COST'));
+exec ops.go(ops.times_ra('HW4_HERO_CARD_STR_CLA_COST','HW4_HERO_CARD_REF_CLA_COST','HW4_HERO_CARD_CLASS_SR'));
+exec ops.go(ops.times_ra('HW4_HERO_CARD_CLASS_SR','HW4_HERO_CARD_RAN_CLA_COST','HW4_HERO_CARD_CLASS_SRR'));
+exec ops.go(ops.times_ra('HW4_HERO_CARD_CLASS_SRR','HW4_HERO_CARD_INT_CLA_COST','HW4_HERO_CARD_CLASS_SRRI'));
+exec ops.go(ops.times_ra('HW4_HERO_CARD_CLASS_SRRI','HW4_HERO_CARD_TEC_CLA_COST','HW4_HERO_CARD_CLASS_SRRIT'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_CLASS_SRRIT','DECK_CARD1=NAME1,DECK_CARD2=NAME2,DECK_CARD3=NAME3,DECK_CARD4=NAME4,DECK_CARD5=NAME5','CLASS1,CLASS2,CLASS3,CLASS4,CLASS5,COST=COST1+COST2+COST3+COST4+COST5','HW4_HERO_CARD_CLASS_MIX_W_COST'));
 
-exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Strength''','JTEST_HERO_CARD_STRENGTH'));
-exec ops.go(ops.reduce_ra('JTEST_HERO_CARD_STRENGTH','NAME1=NAME','COST1=COST,CLASS1=CLASS','JTEST_HERO_CARD_STRENGTH1'));
-exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Reflexes''','JTEST_HERO_CARD_REFLEXES'));
-exec ops.go(ops.reduce_ra('JTEST_HERO_CARD_REFLEXES','NAME2=NAME','COST2=COST,CLASS2=CLASS','JTEST_HERO_CARD_REFLEXES1'));
-exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Ranged''','JTEST_HERO_CARD_RANGED'));
-exec ops.go(ops.reduce_ra('JTEST_HERO_CARD_RANGED','NAME3=NAME','COST3=COST,CLASS3=CLASS','JTEST_HERO_CARD_RANGED1'));
-exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Intuition''','JTEST_HERO_CARD_INTUITION'));
-exec ops.go(ops.reduce_ra('JTEST_HERO_CARD_INTUITION','NAME4=NAME','COST4=COST,CLASS4=CLASS','JTEST_HERO_CARD_INTUITION1'));
-exec ops.go(ops.filter_ra('HERO_CARD','CLASS=''Tech''','JTEST_HERO_CARD_TECH'));
-exec ops.go(ops.reduce_ra('JTEST_HERO_CARD_TECH','NAME5=NAME','COST5=COST,CLASS5=CLASS','JTEST_HERO_CARD_TECH1'));
-exec ops.go(ops.times_ra('JTEST_HERO_CARD_STRENGTH1','JTEST_HERO_CARD_REFLEXES1','JTEST_HERO_CARD_SR'));
-exec ops.go(ops.times_ra('JTEST_HERO_CARD_SR','JTEST_HERO_CARD_RANGED1','JTEST_HERO_CARD_SRR'));
-exec ops.go(ops.times_ra('JTEST_HERO_CARD_SRR','JTEST_HERO_CARD_INTUITION1','JTEST_HERO_CARD_SRRI'));
-exec ops.go(ops.times_ra('JTEST_HERO_CARD_SRRI','JTEST_HERO_CARD_TECH1','JTEST_HERO_CARD_CLASS_MIX'));
-exec ops.go(ops.project_ra('JTEST_HERO_CARD_CLASS_MIX','NAME1,CLASS1,NAME2,CLASS2,NAME3,CLASS3,NAME4,CLASS4,NAME5,CLASS5,COST=COST1+COST2+COST3+COST4+COST5','JTEST_HERO_CARD_CLASS_MIX1'));
+drop table HW4_HERO_CARD_W_RECRUIT;
+drop table HW4_HERO_CARD_W_RECRUIT1;
+drop table HW4_HERO_CARD_W_RECRUIT2;
+drop table HW4_HERO_CARD_W_RECRUIT3;
+drop table HW4_HERO_CARD_W_RECRUIT_C12;
+drop table HW4_HERO_CARD_W_RECRUIT_C123;
+drop table HW4_HERO_CARD_SET3_W_TOT_REC;
+drop table HW4_HERO_CARD_RECRUIT_RES;
+drop table HW4_HERO_CARD_BEST_RECR_SET3;
 
-drop table JTEST_HAND_CARD_WRECRUIT;
-drop table JTEST_HAND_CARD_WRECRUIT1;
-drop table JTEST_HAND_CARD_WRECRUIT2;
-drop table JTEST_HAND_CARD_WRECRUIT3;
-drop table JTEST_HAND_CARD_WRECRUIT_H12;
-drop table JTEST_HAND_CARD_WRECRUIT_H123;
-drop table JTEST_HAND_CARD_TOTAL_RECRUIT;
-
-exec ops.go(ops.filter_ra('HERO_CARD','RECRUIT is not null','JTEST_HAND_CARD_WRECRUIT'));
-exec ops.go(ops.reduce_ra('JTEST_HAND_CARD_WRECRUIT','HAND_CARD_NAME1=NAME','RECRUIT1=RECRUIT','JTEST_HAND_CARD_WRECRUIT1'));
-exec ops.go(ops.reduce_ra('JTEST_HAND_CARD_WRECRUIT','HAND_CARD_NAME2=NAME','RECRUIT2=RECRUIT','JTEST_HAND_CARD_WRECRUIT2'));
-exec ops.go(ops.reduce_ra('JTEST_HAND_CARD_WRECRUIT','HAND_CARD_NAME3=NAME','RECRUIT3=RECRUIT','JTEST_HAND_CARD_WRECRUIT3'));
-exec ops.go(ops.cjoin_ra('JTEST_HAND_CARD_WRECRUIT1','JTEST_HAND_CARD_WRECRUIT2','HAND_CARD_NAME1<HAND_CARD_NAME2','JTEST_HAND_CARD_WRECRUIT_H12'));
-exec ops.go(ops.cjoin_ra('JTEST_HAND_CARD_WRECRUIT_H12','JTEST_HAND_CARD_WRECRUIT3','HAND_CARD_NAME2<HAND_CARD_NAME3','JTEST_HAND_CARD_WRECRUIT_H123'));
-exec ops.go(ops.project_ra('JTEST_HAND_CARD_WRECRUIT_H123','HAND_CARD_NAME1,HAND_CARD_NAME2,HAND_CARD_NAME3,RECRUIT=RECRUIT1+RECRUIT2+RECRUIT3','JTEST_HAND_CARD_TOTAL_RECRUIT'));
-exec ops.go(ops.cjoin_ra('JTEST_HAND_CARD_TOTAL_RECRUIT','JTEST_HERO_CARD_CLASS_MIX1','RECRUIT>=COST and HAND_CARD_NAME1 not in (NAME1,NAME2,NAME3,NAME4,NAME5) and HAND_CARD_NAME2 not in (NAME1,NAME2,NAME3,NAME4,NAME5) and HAND_CARD_NAME3 not in (NAME1,NAME2,NAME3,NAME4,NAME5)','JTEST_RESULT'));
+exec ops.go(ops.filter_ra('HERO_CARD','RECRUIT is not null','HW4_HERO_CARD_W_RECRUIT'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_W_RECRUIT','HAND_CARD1=NAME','RECRUIT1=RECRUIT','HW4_HERO_CARD_W_RECRUIT1'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_W_RECRUIT','HAND_CARD2=NAME','RECRUIT2=RECRUIT','HW4_HERO_CARD_W_RECRUIT2'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_W_RECRUIT','HAND_CARD3=NAME','RECRUIT3=RECRUIT','HW4_HERO_CARD_W_RECRUIT3'));
+exec ops.go(ops.cjoin_ra('HW4_HERO_CARD_W_RECRUIT1','HW4_HERO_CARD_W_RECRUIT2','HAND_CARD1<HAND_CARD2','HW4_HERO_CARD_W_RECRUIT_C12'));
+exec ops.go(ops.cjoin_ra('HW4_HERO_CARD_W_RECRUIT_C12','HW4_HERO_CARD_W_RECRUIT3','HAND_CARD2<HAND_CARD3','HW4_HERO_CARD_W_RECRUIT_C123'));
+exec ops.go(ops.project_ra('HW4_HERO_CARD_W_RECRUIT_C123','HAND_CARD1,HAND_CARD2,HAND_CARD3,RECRUIT=RECRUIT1+RECRUIT2+RECRUIT3','HW4_HERO_CARD_SET3_W_TOT_REC'));
+exec ops.go(ops.cjoin_ra('HW4_HERO_CARD_SET3_W_TOT_REC','HW4_HERO_CARD_CLASS_MIX_W_COST','RECRUIT>=COST and HAND_CARD1 not in (DECK_CARD1,DECK_CARD2,DECK_CARD3,DECK_CARD4,DECK_CARD5) and HAND_CARD2 not in (DECK_CARD1,DECK_CARD2,DECK_CARD3,DECK_CARD4,DECK_CARD5) and HAND_CARD3 not in (DECK_CARD1,DECK_CARD2,DECK_CARD3,DECK_CARD4,DECK_CARD5)','HW4_HERO_CARD_RECRUIT_RES'));
+exec ops.go(ops.reduce_ra('HW4_HERO_CARD_RECRUIT_RES','HAND_CARD1,HAND_CARD2,HAND_CARD3','HW4_HERO_CARD_BEST_RECR_SET3'));
 /**/
