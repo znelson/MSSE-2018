4254b33dafe36fe9dc9a88128c6f45b8acbdcfd2
Chi Yung Fung (Jimmy) <jimmythefung@users.noreply.github.com>
2016-12-14 19:43:43 -0600

    Update Jimmy_Query.sql

--- 5707/HW4/Jimmy_Query.sql
+++ 5707/HW4/Jimmy_Query.sql
@@ -1,7 +1,7 @@
---1. How many 5-hero combos are there that contain at least one hero from each team?
+--1. How many hero combos are there that contain at least one hero from each team?
 DROP TABLE JIM_1H_FROM_EACH_TEAM;
 DROP TABLE JIM_1H_FROM_EACH_TEAM_PROJECT;
-exec ops.go(ops.filter_ra('HW4_HERO_COMBO_W_ALL_STAT','HERO_COUNT=5 AND AVENGERS_CARD_TYPE_COUNT>0 AND SPIDERFRIENDS_CARD_TYPE_COUNT>0 AND XMEN_CARD_TYPE_COUNT>0 AND SHIELD_CARD_TYPE_COUNT>0','JIM_1H_FROM_EACH_TEAM'));
+exec ops.go(ops.filter_ra('HW4_HERO_COMBO_W_ALL_STAT','AVENGERS_CARD_TYPE_COUNT>0 AND SPIDERFRIENDS_CARD_TYPE_COUNT>0 AND XMEN_CARD_TYPE_COUNT>0 AND SHIELD_CARD_TYPE_COUNT>0','JIM_1H_FROM_EACH_TEAM'));
 exec ops.go(ops.project_ra('JIM_1H_FROM_EACH_TEAM','COMBO_ID, HERO_COUNT, AVENGERS_CARD_TYPE_COUNT, SPIDERFRIENDS_CARD_TYPE_COUNT, XMEN_CARD_TYPE_COUNT, SHIELD_CARD_TYPE_COUNT','JIM_1H_FROM_EACH_TEAM_PROJECT'));
 
 
@@ -11,12 +11,30 @@ exec ops.go(ops.project_ra('JIM_1H_FROM_EACH_TEAM','COMBO_ID, HERO_COUNT, AVENGE
 DROP TABLE JIM_1C_FROM_EACH_TEAM;
 DROP TABLE JIM_1C_FROM_EACH_TEAM_PROJECT;
 exec ops.go(ops.filter_ra('HW4_HERO_COMBO_W_ALL_STAT','REFLEXES_CARD_TYPE_COUNT>0 AND STRENGTH_CARD_TYPE_COUNT>0 AND RANGED_CARD_TYPE_COUNT>0 AND INTUITION_CARD_TYPE_COUNT>0 AND TECH_CARD_TYPE_COUNT>0','JIM_1C_FROM_EACH_TEAM'));
-exec ops.go(ops.project_ra('JIM_1H_FROM_EACH_TEAM','COMBO_ID, HERO_COUNT, REFLEXES_CARD_TYPE_COUNT, STRENGTH_CARD_TYPE_COUNT, RANGED_CARD_TYPE_COUNT, INTUITION_CARD_TYPE_COUNT, TECH_CARD_TYPE_COUNT','JIM_1C_FROM_EACH_TEAM_PROJECT'));
+exec ops.go(ops.project_ra('JIM_1C_FROM_EACH_TEAM','COMBO_ID, HERO_COUNT, REFLEXES_CARD_TYPE_COUNT, STRENGTH_CARD_TYPE_COUNT, RANGED_CARD_TYPE_COUNT, INTUITION_CARD_TYPE_COUNT, TECH_CARD_TYPE_COUNT','JIM_1C_FROM_EACH_TEAM_PROJECT'));
 
 
 
 
---3. How many hero combos are there that contain exactly 1 team? (What about 0 team?)
+--3. How many 5 hero combos are there that contain at least one hero from each team and one card of each color?
+DROP TABLE JIM_1H_1C_FROM_EACH_TEAM;
+DROP TABLE JIM_1H1C_5COMBO;
+exec ops.go(ops.intersect_ra('JIM_1H_FROM_EACH_TEAM', 'JIM_1C_FROM_EACH_TEAM', 'JIM_1H_1C_FROM_EACH_TEAM'));
+exec ops.go(ops.filter_ra('JIM_1H_1C_FROM_EACH_TEAM','HERO_COUNT=5','JIM_1H1C_5COMBO'));
+
+
+
+
+--4. How many 6-hero combos are there that are gender balance (i.e. 3 females in 6-combo) and 3 heros from a single team?
+DROP TABLE JIM_GENDER_BALANCED_COMBO;
+exec ops.go(ops.filter_ra('HW4_HERO_COMBO_W_ALL_STAT','HERO_COUNT=6 AND FEMALE_HERO_COUNT=3 AND (AVENGERS_CARD_TYPE_COUNT=12 OR SPIDERFRIENDS_CARD_TYPE_COUNT=12 XMEN_CARD_TYPE_COUNT=12 OR SHIELD_CARD_TYPE_COUNT=12)','JIM_TEMP1'));
+exec ops.go(ops.project_ra('JIM_TEMP1','COMBO_ID, FEMALE_HERO_COUNT, AVENGERS_CARD_TYPE_COUNT, SPIDERFRIENDS_CARD_TYPE_COUNT, XMEN_CARD_TYPE_COUNT, SHIELD_CARD_TYPE_COUNT','JIM_GENDER_BALANCED_COMBO'));
+DROP TABLE JIM_TEMP1;
+
+
+
+
+--5. How many hero combos are there that contain exactly 1 team? (What about 0 team?)
 DROP TABLE JIM_ONE_TEAM_COMBO;
 DROP TABLE JIM_ONE_TEAM_COMBO_PROJECT;
 DECLARE
@@ -36,7 +54,7 @@ exec ops.go(ops.project_ra('JIM_ONE_TEAM_COMBO','COMBO_ID, HERO_COUNT, AVENGERS_
 
 
 
---4. How many hero combos are there that have exactly 1 color? (or 2, 3..etc. ?)
+--6. How many hero combos are there that have exactly 1 color? (or 2, 3..etc. ?)
 DROP TABLE JIM_ONE_COLOR_COMBO;
 DROP TABLE JIM_ONE_COLOR_COMBO_PROJECT;
 DECLARE
@@ -57,7 +75,7 @@ exec ops.go(ops.project_ra('JIM_ONE_COLOR_COMBO','COMBO_ID, HERO_COUNT, REFLEXES
 
 
 
---5. What is the “REDDEST” 5,6-hero combo (perhaps with ties)? (This could mean different things, with tiebreaker conditions)
+--7. What is the “REDDEST” 5,6-hero combo (perhaps with ties)? (This could mean different things, with tiebreaker conditions)
 DROP TABLE JIM_RED_COMBO;
 DROP TABLE JIM_REDDEST_5COMBO_COUNT;
 DROP TABLE JIM_REDDEST_6COMBO_COUNT;
@@ -86,12 +104,3 @@ exec ops.go(ops.mjoin_ra('JIM_RED_COMBO', 'JIM_REDDEST_6COMBO_COUNT', 'PRODUCT6'
 exec ops.go(ops.union_ra('JIM_REDDEST_5COMBO', 'JIM_REDDEST_6COMBO', 'JIM_REDDEST_56COMBO'));
 exec ops.go(ops.project_ra('JIM_REDDEST_56COMBO', 'REFLEXES_CARD_COUNT, REFLEXES_CARD_TYPE_COUNT, HERO_COUNT, COLOR_PRODUCT=(case when PRODUCT5=0 then PRODUCT6 else PRODUCT5 end)', 'JIM_REDDEST_56COMBO_PROJECT'));
 exec ops.go(ops.mjoin_ra('JIM_REDDEST_56COMBO_PROJECT', 'HW4_HERO_COMBO_W_ALL_STAT', 'REFLEXES_CARD_COUNT, REFLEXES_CARD_TYPE_COUNT, HERO_COUNT', 'REFLEXES_CARD_COUNT, REFLEXES_CARD_TYPE_COUNT, HERO_COUNT', 'JIM_REDDEST_ALL_COMBO'));
-
-
-
-
---6. How many 6- hero combos are there that are gender balance (3 females in 6-combo)?
-DROP TABLE JIM_GENDER_BALANCED_COMBO;
-exec ops.go(ops.filter_ra('HW4_HERO_COMBO_W_ALL_STAT','HERO_COUNT=6 AND FEMALE_HERO_COUNT=3','JIM_TEMP1'));
-exec ops.go(ops.project_ra('JIM_TEMP1','COMBO_ID, FEMALE_HERO_COUNT','JIM_GENDER_BALANCED_COMBO'));
-DROP TABLE JIM_TEMP1;
